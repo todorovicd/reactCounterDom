665a2a83f654ca591ee06d17061d4117e334b8aa
todorovicd <dt9669@moval.edu>
2019-07-12 15:32:52 -0500

    add navbar counter, sync copmponents - lift state, lifecycle hooks

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar
-        </a>
-      </nav>
-    );
-  }
-}
+const Navbar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge nadge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default Navbar;
