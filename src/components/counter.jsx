9fdf7df9df02706a7371661cf4201ea68d5ab831
todorovicd <dt9669@moval.edu>
2019-07-12 11:48:05 -0500

    add counter and counters components, navbar, update components tree in the index.js

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,24 +1,22 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 0
-  };
-
-  handleIncrement = product => {
-    console.log(product);
-    this.setState({ count: this.state.count + 1 });
-  };
-
   render() {
     return (
       <div>
+        {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement(product)}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
         <ul>{/*    */}</ul>
       </div>
@@ -27,12 +25,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.props.counter;
     return count === 0 ? "Zero" : count;
   }
 }
